a13bf86574c6f538864683b6fa20eabfbfc57e89
Salim <kimidzono@gmail.com>
2025-07-23 21:03:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = 'robotics-v1';
+const CACHE_NAME = 'robotika-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
-  '/scripts.js',
-  '/icons/icon-192x192.jpg'
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then(response => response || fetch(event.request))
   );
 });
